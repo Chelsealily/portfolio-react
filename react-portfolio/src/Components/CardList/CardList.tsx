a7f469273252791458f64d305fba5e83b268ad5c
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2024-02-22 16:55:54 +0000

    added cardlist component

---
+++ react-portfolio/src/Components/CardList/CardList.tsx
@@ -0,0 +1,21 @@
+import "./CardList.scss"
+import { Link } from "react-router-dom";
+import { ProjectType} from "../../types/types"
+import Card from "../Card/Card";
+
+type CardListProps ={
+  projects:ProjectType[];
+}
+
+const CardList = ({projects}:CardListProps) => { 
+  return ( 
+    <div className="card-list">
+       {projects.map((project) => (
+        <Link key={project.id} to={`/project/${project.id}`}>
+      <Card key={project.id} project={project}/>
+       </Link>
+  ))};
+  </div>
+);
+};
+export default CardList;
